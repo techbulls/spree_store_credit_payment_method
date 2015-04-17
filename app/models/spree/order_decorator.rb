60a880fdea3db516ff143f7907a970815c968cac
Corey Reece <corey@mayvenn.com>
2015-04-17 13:07:22 -0700

    Remove transition callback to automatically apply store credit

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,5 @@
 module SpreeStoreCredits::OrderDecorator
   def self.included(base)
-    base.state_machine.before_transition to: :confirm, do: :add_store_credit_payments
     base.state_machine.after_transition to: :confirm, do: :create_gift_cards
 
     base.prepend(InstanceMethods)
