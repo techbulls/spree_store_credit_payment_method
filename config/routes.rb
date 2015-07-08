a62d530c061b929e8334ca23bfcd1bede495a32d
kunal herkal <kunalrherkal@gmail.com>
2015-07-08 12:41:28 +0530

    Added routes for loyalty points.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Spree::Core::Engine.routes.draw do
+
+  resources :loyalty_points, only: [:index]
+
+  namespace :admin do
+    resources :users do
+      resources :loyalty_points, only: [:index, :new, :create], controller: 'loyalty_points_transactions' do
+        get 'order_transactions/:order_id', action: :order_transactions, on: :collection
+      end
+    end
+  end
+  
   namespace :admin do
     resources :users, only: [] do
       resources :store_credits
