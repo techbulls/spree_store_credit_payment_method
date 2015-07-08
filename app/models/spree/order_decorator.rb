853ef5b3daed99b418d2e690ea4fbcc762d34802
kunal herkal <kunalrherkal@gmail.com>
2015-07-08 14:53:13 +0530

    Changes in Order decorator.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,18 @@
 module SpreeStoreCredits::OrderDecorator
+
+  has_many :loyalty_points_transactions, as: :source
+  has_many :loyalty_points_credit_transactions, as: :source
+  has_many :loyalty_points_debit_transactions, as: :source
+
+  scope :loyalty_points_not_awarded, -> { includes(:loyalty_points_credit_transactions).where(:spree_loyalty_points_transactions => { :source_id => nil } ) }
+
+  scope :with_hours_since_payment, ->(num) { where('`spree_orders`.`paid_at` < ? ', num.minutes.ago) }
+
+  scope :with_uncredited_loyalty_points, ->(num) { with_hours_since_payment(num).loyalty_points_not_awarded }
+
+  fsm = self.state_machines[:state]
+  fsm.before_transition :from => fsm.states.map(&:name) - [:complete], :to => [:complete], :do => :complete_loyalty_points_payments
+
   def self.included(base)
     base.state_machine.before_transition to: :confirm, do: :add_store_credit_payments
     #base.state_machine.after_transition to: :confirm, do: :create_gift_cards
