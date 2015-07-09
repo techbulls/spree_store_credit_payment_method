4126e25ef906910f3d383c5c5cb4440a2d3c3426
kunal herkal <kunalrherkal@gmail.com>
2015-07-09 14:27:27 +0530

    Really dding loyalty points when referred user registers.

--- app/controllers/spree/user_controller_decorator.rb
+++ app/controllers/spree/user_controller_decorator.rb
@@ -1,15 +1,17 @@
 Spree::UserRegistrationsController.class_eval do
-  after_filter :after_create, :only => :create
-  def after_create
-  	puts "********************** in after_create method"
-  	referred_records = Spree::ReferredRecord.where(:user_id => current_user.id)
+  after_filter :credit_loyalty_points_to_referral_user, :only => :create
+  
+  def credit_loyalty_points_to_referral_user
+  	referred_records = Spree::ReferredRecord.where(:user_id => @user.id)
   	referred_records.each do |referred_record|
-  		puts "INSIDE LOOP"
-  		referral_user_id = referred_record.referral_id
-  		referral_user = Spree::User.find_by(:id => referral_user_id)
-  		referral_user.loyalty_points_balance = referral_user.loyalty_points_balance + Spree::Config.loyalty_points_per_referred_user
-  		referred_record.loyalty_points_awarded = true
-  		referred_record.save!
+  		if !referred_record.loyalty_points_awarded
+	  		referral_user_id = referred_record.referral_id
+	  		referral_user = Spree::User.find_by(:id => referral_user_id)
+	  		referral_user.loyalty_points_balance += Spree::Config.loyalty_points_per_referred_user
+			referral_user.save!
+	  		referred_record.loyalty_points_awarded = true
+	  		referred_record.save!
+  		end
   	end	
   end
 end
